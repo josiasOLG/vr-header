ae62c96aebcd8681280220c1a386305de7e82f40
josias <josiasoliveira111@gmail.com>
2025-08-01 00:48:07 -0300

    fix(): start

---
+++ components/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Microfrontend Header</h1>
+      <p>Este é o Microfrontend rodando dentro do Shell!</p>
+    </div>
+  );
+};
+
+export default App;
